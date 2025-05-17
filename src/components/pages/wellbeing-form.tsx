25cdf7ed53083d8010a97a9819804527fc0dcf2b
valentypo <stevenv2605@gmail.com>
2025-05-17 19:38:53 +0700

    fixed response
    forgot to add ` lol

--- src/components/pages/wellbeing-form.tsx
+++ src/components/pages/wellbeing-form.tsx
@@ -211,7 +211,7 @@ export default function WellbeingForm() {
         financialStress: Number.parseInt(formData.financialStress),
       }
 
-      const response = await fetch(${process.env.NEXT_PUBLIC_API_URL}/api/predict, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/predict`, {
         method: 'POST',
         body: JSON.stringify(data),
         headers: { 'Content-Type': 'application/json' }
