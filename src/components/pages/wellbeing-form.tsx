8033f5d568f1914b101037e718f1f13de163e736
valentypo <stevenv2605@gmail.com>
2025-06-19 21:54:05 +0700

    changed url of backend

--- src/components/pages/wellbeing-form.tsx
+++ src/components/pages/wellbeing-form.tsx
@@ -186,7 +186,7 @@ export default function WellbeingForm() {
         financialStress: Number.parseInt(formData.financialStress),
       }
 
-      const response = await fetch("https://depression-learning-backend-production.up.railway.app/api/predict", {
+      const response = await fetch("http://machine-learning-production-76a6.up.railway.app/api/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
